8fb5894418b9e8b5726fa23ddd6d7b27c86fc5dc
EC2 Default User <ec2-user@ip-172-31-28-49.ap-northeast-1.compute.internal>
2022-06-25 17:23:36 +0000

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # name：一意性を持たせ、かつ2～20文字の範囲で設定してください。
+  validates:name,
+    presence:true,
+    length:{minimum:2,maximum:20},
+    uniqueness:true
+  # introduction：最大50文字までに設定してください。
+  validates:introduction,
+    length:{maximum:50}
+
+  has_many :books,dependent: :destroy
+  has_one_attached :profile_image
+  def get_profile_image(x,y)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
+end
